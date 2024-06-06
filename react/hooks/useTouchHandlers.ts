f33bb94289fa3ef6f6bd01ea8c56bc5450e1ca04
Gustavo <gurosano@gmail.com>
2024-06-06 17:24:33 -0300

    feat: adjust in vertical scroll of sliders

--- react/hooks/useTouchHandlers.ts
+++ react/hooks/useTouchHandlers.ts
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-
 import { useSliderControls } from './useSliderControls'
 import {
   SliderLayoutProps,
@@ -23,46 +22,72 @@ export const useTouchHandlers = ({
 
   const [touchState, setTouchState] = useState({
     touchStartX: 0,
+    touchStartY: 0,
     touchInitialTransform: transform,
+    isVerticalScroll: undefined as boolean | undefined,
   })
 
   const onTouchStart = (e: React.TouchEvent) => {
     const startX = e.touches[0].clientX
+    const startY = e.touches[0].clientY
 
-    setTouchState({ touchStartX: startX, touchInitialTransform: transform })
+    setTouchState({
+      touchStartX: startX,
+      touchStartY: startY,
+      touchInitialTransform: transform,
+      isVerticalScroll: undefined,
+    })
   }
 
   const onTouchMove = (e: React.TouchEvent) => {
     const currentX = e.touches[0].clientX
-    const touchMoveDelta = currentX - touchState.touchStartX
+    const currentY = e.touches[0].clientY
+    const xDiff = touchState.touchStartX - currentX
+    const yDiff = touchState.touchStartY - currentY
 
-    const newTransform =
-      touchState.touchInitialTransform +
-      touchMoveDelta /
-        ((centerMode !== 'disabled' ? 2 : 1) * TOUCH_MOVE_DAMPING)
+    if (touchState.isVerticalScroll === undefined) {
+      const isVertical = Math.abs(yDiff) > Math.abs(xDiff)
+      setTouchState(prevState => ({
+        ...prevState,
+        isVerticalScroll: isVertical,
+      }))
+      if (isVertical) {
+        return
+      }
+    }
+
+    if (touchState.isVerticalScroll) {
+      return
+    }
+
+    const touchMoveDelta = currentX - touchState.touchStartX
+    const newTransform = touchState.touchInitialTransform + touchMoveDelta / ((centerMode !== 'disabled' ? 2 : 1) * TOUCH_MOVE_DAMPING)
 
     dispatch({
       type: 'TOUCH',
       payload: { transform: newTransform, isOnTouchMove: true },
     })
+
+    e.preventDefault()
   }
 
   const onTouchEnd = (e: React.TouchEvent) => {
+    if (touchState.isVerticalScroll) {
+      return
+    }
+
     const endX = e.changedTouches[0].clientX
     const delta = endX - touchState.touchStartX
 
     if (Math.abs(delta) > SWIPE_THRESHOLD) {
-      // Swipe from left to right
       if (delta > 0) {
         goBack()
       }
 
-      // Swipe from right to left
       if (delta < 0) {
         goForward()
       }
     } else {
-      // Ignore the swipe if SWIPE_THRESHOLD is not reached
       dispatch({
         type: 'TOUCH',
         payload: {
@@ -72,7 +97,7 @@ export const useTouchHandlers = ({
       })
     }
 
-    setTouchState({ touchStartX: 0, touchInitialTransform: transform })
+    setTouchState({ touchStartX: 0, touchStartY: 0, touchInitialTransform: transform, isVerticalScroll: undefined })
     dispatch({
       type: 'TOUCH',
       payload: {
